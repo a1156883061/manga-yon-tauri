364c04eb09a061217ed5c4402361b77d1f5c4c2c
TheCrown <a1156883061@outlook.com>
2023-07-14 18:18:58 +0800

    feat(biz): 删除老版代码

--- src/config/store.ts
+++
@@ -1,8 +0,0 @@
-import { app } from 'electron';
-import Store from 'electron-store';
-import path from 'path';
-
-const store = new Store({
-  cwd: path.join(app.getPath('userData'), 'config'),
-});
-export { store };
